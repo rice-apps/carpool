cd1dd68b062ec27d3d445662f2a5eb603111da64
fdrozdowski <fgd1@rice.edu>
2015-10-12 23:16:05 -0500

    Cleaned up more

--- db_repository/manage.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from migrate.versioning.shell import main
-
-if __name__ == '__main__':
-    main(six='<module 'six' from 'C:\Users\CBurk\microblog\venv\lib\site-packages\six.pyc'>')
